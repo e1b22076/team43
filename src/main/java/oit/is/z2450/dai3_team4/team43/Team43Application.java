0cd2ae7c21cb9bae2faad8e29607bfe9d67ad372
e1b22076 <e1b22076@oit.ac.jp>
2024-10-08 14:08:34 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z2450/dai3_team4/team43/Team43Application.java
@@ -0,0 +1,13 @@
+package oit.is.z2450.dai3_team4.team43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team43Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team43Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z2450/dai3_team4/team43/Team43ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z2450.dai3_team4.team43;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team43ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
