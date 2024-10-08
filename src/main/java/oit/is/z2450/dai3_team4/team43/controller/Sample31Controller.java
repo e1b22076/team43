5f2f6c6577dd246cffd359f0e5cb1ea7ac1ae3b8
Yura-Mp <e1b22079@oit.ac.jp>
2024-10-08 15:10:50 +0900

    sample3-5完了

---
+++ src/main/java/oit/is/z2450/dai3_team4/team43/controller/Sample31Controller.java
@@ -0,0 +1,43 @@
+package oit.is.z2450.dai3_team4.team43.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// import oit.is.inudaisuki.springboot_samples.model.Room;
+
+/**
+ * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
+ */
+@Controller
+@RequestMapping("/sample3")
+public class Sample31Controller {
+  @GetMapping("step1")
+  public String sample31() {
+    return "sample31.html";
+  }
+
+  @GetMapping("step3")
+  public String sample33() {
+    return "sample33.html";
+  }
+
+  /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   */
+  @GetMapping("step2")
+  public String sample32(ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
+    return "sample31.html";
+  }
+}
