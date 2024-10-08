615f0e5c9958123120d07e5776d35c59331f24b3
e1b22076 <e1b22076@oit.ac.jp>
2024-10-08 15:57:59 +0900

    チーム課題終わり

--- src/main/java/oit/is/z2450/dai3_team4/team43/controller/Sample31Controller.java
+++ src/main/java/oit/is/z2450/dai3_team4/team43/controller/Sample31Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// import oit.is.inudaisuki.springboot_samples.model.Room;
+import oit.is.z2450.dai3_team4.team43.model.Room;
 
 /**
  * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
@@ -18,6 +18,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/sample3")
 public class Sample31Controller {
+
+  @Autowired
+  private Room room;
+
   @GetMapping("step1")
   public String sample31() {
     return "sample31.html";
@@ -26,6 +30,11 @@ public class Sample31Controller {
   @GetMapping("step3")
   public String sample33() {
     return "sample33.html";
+  }
+
+  @GetMapping("step7")
+  public String sample37() {
+    return "sample37.html";
   }
 
   /**
@@ -46,6 +55,25 @@ public class Sample31Controller {
     int kekka = hiku1 - hiku2;
     model.addAttribute("hikukekka", kekka);
     return "sample33.html";
+  }
+
+  @GetMapping("step8")
+  public String sample38(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
+    return "sample37.html";
+  }
+
+  @GetMapping("step9")
+  public String sample39(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    Room newRoom = new Room();
+    newRoom.addUser(loginUser);
+    model.addAttribute("new_room", newRoom);
+
+    return "sample37.html";
   }
 
 }
